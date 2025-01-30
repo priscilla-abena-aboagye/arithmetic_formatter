c58d66e8130731a7c6f7232edb84de85d5640c31
Abena <priscillaabenaaboagye@gmail.com>
2025-01-30 20:50:56 +0000

    the print statement

--- formatter.py
+++ formatter.py
@@ -47,3 +47,13 @@ def arithmetic_arranger(problems, show_answers=False):
     return arranged
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
+print(arithmetic_arranger(["1 + 2", "1 - 9380"]))
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]))
+print(arithmetic_arranger(["44 + 815", "909 - 2", "45 + 43", "123 + 49", "888 + 40", "653 + 87"]))
+print(arithmetic_arranger(["3 / 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["24 + 85215", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["98 + 3g5", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["3 + 855", "988 + 40"], True))
+print(arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True))

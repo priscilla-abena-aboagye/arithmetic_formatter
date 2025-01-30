1471345db5f7e74f6fcecc9f96ad20fbe6dc2024
Abena <priscillaabenaaboagye@gmail.com>
2025-01-30 20:50:20 +0000

    working on making it function

--- formatter.py
+++ formatter.py
@@ -7,7 +7,7 @@ def arithmetic_arranger(problems, show_answers=False):
     dash = ""
     answer = ""
 
-    for problem in problems:
+    for i, problem in enumerate(problems):
         parts = problem.split()
 
         num1, operators, num2 = parts
@@ -25,23 +25,25 @@ def arithmetic_arranger(problems, show_answers=False):
             return 'Error: Numbers cannot be more than four digits.'
 
         width = max(len(num1), len(num2)) + 2
-        first += num1.rjust(width) + " "
-        second += operators + num2.rjust(width - 1) + " "
-        dash = "-" * width + "  "
+
+        space = "    " if i < len(problems) - 1 else ""
+
+        first += num1.rjust(width) + space
+
+        second += operators + " " + num2.rjust(width - 2) + space
+
+        dash += "-" * width + space
 
         if show_answers:
             answers = str(eval(num1 + operators + num2))
-            answer += answers.rjust(width) + " "
+            answer += answers.rjust(width) + space
 
     arranged = first.rstrip() + "\n" + second.rstrip() + "\n" + dash.rstrip()
+
     if show_answers:
         arranged += "\n" + answer.rstrip()
+
+
     return arranged
 
-
- 
-        
-
-    return problems
-
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))

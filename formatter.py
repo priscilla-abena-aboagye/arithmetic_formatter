6856571cdd7ee46a5cf76b2ba6af3cc461655df9
Abena <priscillaabenaaboagye@gmail.com>
2025-01-30 19:18:38 +0000

    formatting the code

--- formatter.py
+++ formatter.py
@@ -2,14 +2,32 @@ def arithmetic_arranger(problems, show_answers=False):
 
     if len(problems) > 5:
         return 'Error: Too many problems.'
+    first = ""
+    second = ""
+    dash = ""
+    answer = ""
 
-    operators = ["+", "-"]
-    if operators not in operators:
-        return "Error: Operator must be '+' or '-'."
-    for number in arithmetic_arranger:
-        pass
-    else:
-        return "Error: Operator must be '+' or '-'."
+    for problem in problems:
+        parts = problem.split()
+
+        num1, operators, num2 = parts
+
+        operators = ["+", "-"]
+
+        if operators not in operators:
+            return "Error: Operator must be '+' or '-'."
+    
+        if not (num1.isdigit() and num2.isdigit()):
+            return 'Error: Numbers must only contain digits.'
+
+
+        if len(num1) > 4 or len(num2) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+
+        width = max(len(num1), len(num2)) + 1
+
+
+ 
         
 
     return problems

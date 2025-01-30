8a5b9d19f2339e020b7e46264b9b827c4a5382ee
Abena <priscillaabenaaboagye@gmail.com>
2025-01-30 19:55:40 +0000

    arranging the operations

--- formatter.py
+++ formatter.py
@@ -12,9 +12,9 @@ def arithmetic_arranger(problems, show_answers=False):
 
         num1, operators, num2 = parts
 
-        operators = ["+", "-"]
-
-        if operators not in operators:
+        if operators in ["+", "-"]:
+            pass
+        else:
             return "Error: Operator must be '+' or '-'."
     
         if not (num1.isdigit() and num2.isdigit()):
@@ -24,7 +24,19 @@ def arithmetic_arranger(problems, show_answers=False):
         if len(num1) > 4 or len(num2) > 4:
             return 'Error: Numbers cannot be more than four digits.'
 
-        width = max(len(num1), len(num2)) + 1
+        width = max(len(num1), len(num2)) + 2
+        first += num1.rjust(width) + " "
+        second += operators + num2.rjust(width - 1) + " "
+        dash = "-" * width + "  "
+
+        if show_answers:
+            answers = str(eval(num1 + operators + num2))
+            answer += answers.rjust(width) + " "
+
+    arranged = first.rstrip() + "\n" + second.rstrip() + "\n" + dash.rstrip()
+    if show_answers:
+        arranged += "\n" + answer.rstrip()
+    return arranged
 
 
  
